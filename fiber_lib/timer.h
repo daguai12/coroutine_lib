6b48105cf911c2d33238aac0fa878249aae4bc1c
daguai <13935474921@163.com>
2025-07-05 14:56:35 +0800

    添加定时器模块

--- fiber_lib/timer.h
+++ fiber_lib/timer.h
@@ -1,6 +1,8 @@
 #ifndef _TIMER_H_
 #define _TIMER_H_
 
+#include <chrono>
+#include <cstdint>
 #include <memory>
 #include <vector>
 #include <set>
@@ -9,24 +11,42 @@
 #include <functional>
 #include <mutex>
 
-namespace sylar {
+namespace sylar 
+{
 
 class TimerManager;
 
-class Timer : public std::enable_shared_from_this<Timer> 
+class Timer : public std::enable_shared_from_this<Timer>
 {
     friend class TimerManager;
 public:
-    // 从时间堆中删除timer
+    /**
+    * @brief 从时间堆中删除timer
+    */
     bool cancel();
-    // 刷新timer
+
+    /**
+    * @brief 刷新timer
+    */
     bool refresh();
-    // 重设timer的超时时间
+
+    /**
+    * @brief重设timer的超时时间
+    * @param[in] ms 定时器执行间隔时间(毫秒)
+    * @param[in] from_now 是否从当前时间开始计算
+    */
     bool reset(uint64_t ms, bool from_now);
 
 private:
+    /**
+     * @brief 构造函数
+     * @param[in] ms 定时器执行间隔时间
+     * @param[in] cb 回调函数
+     * @param[in] recurring 是否循环
+     * @param[in] manager 定时器管理器
+     */
     Timer(uint64_t ms, std::function<void()> cb, bool recurring, TimerManager* manager);
- 
+
 private:
     // 是否循环
     bool m_recurring = false;
@@ -34,48 +54,86 @@ private:
     uint64_t m_ms = 0;
     // 绝对超时时间
     std::chrono::time_point<std::chrono::system_clock> m_next;
-    // 超时时触发的回调函数
+    // 超时时触发的回掉函数
     std::function<void()> m_cb;
     // 管理此timer的管理器
     TimerManager* m_manager = nullptr;
 
 private:
-    // 实现最小堆的比较函数
-    struct Comparator 
+    /**
+     * @brief实现最小堆的比较函数
+     */
+    struct Comparator
     {
+        /**
+         * @brief 比较定时器的智能指针的大小(按执行时间排序)
+         * @param[in] lsh 定时器智能指针
+         * @param[in] rhs 定时器智能指针
+         */
         bool operator()(const std::shared_ptr<Timer>& lhs, const std::shared_ptr<Timer>& rhs) const;
     };
 };
 
-class TimerManager 
+
+/**
+ * @brief 定时器管理器
+ */
+
+class TimerManager
 {
     friend class Timer;
 public:
+    /**
+     * @brief 构造函数
+     */
     TimerManager();
+    
+    /**
+     * @brief 析构函数
+     */
     virtual ~TimerManager();
 
-    // 添加timer
+    /**
+    * @brief添加timer
+    * @param[in] ms 定时器执行间隔时间
+    * @param[in] cb 定时器回调函数
+    * @param[in] recurring 是否循环定时器
+    */
     std::shared_ptr<Timer> addTimer(uint64_t ms, std::function<void()> cb, bool recurring = false);
 
-    // 添加条件timer
-    std::shared_ptr<Timer> addConditionTimer(uint64_t ms, std::function<void()> cb, std::weak_ptr<void> weak_cond, bool recurring = false);
-
-    // 拿到堆中最近的超时时间
+    /**
+    * @brief 添加条件timer
+    * @param[in] ms 定时器执行间隔时间
+    * @param[in] cb 定时器回调函数
+    * @param[in] weak_cond条件
+    * @param[in] recurring 是否循环
+    */
+    std::shared_ptr<Timer> addConditionTimer(uint64_t ms, std::function<void()> cb, std::weak_ptr<void> weak_cond,bool recurring = false);
+    
+    /**
+    * @brief 拿到堆中最近的超时时间
+    */
     uint64_t getNextTimer();
 
-    // 取出所有超时定时器的回调函数
+    /**
+    * @brief 取出所有超时定时器的回调函数
+    * @param[out] cbs 回调函数数组
+    */
     void listExpiredCb(std::vector<std::function<void()>>& cbs);
 
-    // 堆中是否有timer
+    //堆中是否有timer
     bool hasTimer();
 
 protected:
-    // 当一个最早的timer加入到堆中 -> 调用该函数
-    virtual void onTimerInsertedAtFront() {};
+    /**
+    * @brief 当有新的定时器插入到定时器的首部，执行该函数
+    */
+    virtual void onTimerInsertedAtFront() {}
 
-    // 添加timer
+    /**
+    * @brief 将定时器添加到管理器中
+    */
     void addTimer(std::shared_ptr<Timer> timer);
-
 private:
     // 当系统时间改变时 -> 调用该函数
     bool detectClockRollover();
@@ -90,6 +148,9 @@ private:
     std::chrono::time_point<std::chrono::system_clock> m_previouseTime;
 };
 
-}
+
+};
+
+
 
 #endif
